68f21df76e10566595c59afec5e76108ed7b6079
Wee Seng <wee.bscl@gmail.com>
2023-03-15 00:39:14 +0800

    formArray test

--- src/app/components/cart.component.ts
+++ src/app/components/cart.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Customer, LineItem } from './models';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnChanges {
 
   cartForm!: FormGroup
-  itemArray!: FormArray
 
   @Input()
   cart!: LineItem[]
@@ -27,13 +26,32 @@ export class CartComponent implements OnInit {
       name: this.fb.control<string>('', [Validators.required]),
       address: this.fb.control<string>('', [Validators.required]),
       delivery: this.fb.control<string>('', [Validators.required]),
-      cart: this.itemArray,
+      cart: this.fb.array([]),
       total: this.fb.control<number>(0)
     })
   }
 
+  ngOnChanges(): void {
+    // hmm how
+  }
+
   checkout() {
+    let total: number = 0
+    const totalControl = this.cartForm.get('total') as FormControl
     const cartArray = this.cartForm.get('cart') as FormArray
+    cartArray.clear() // just in case
+    // populate form array
+    this.cart.forEach((li: LineItem) => {
+      cartArray.push(this.fb.group({
+        item: li.item,
+        unitPrice: li.unitPrice,
+        qty: li.qty,
+        subtotal: li.subtotal
+      }))
+      // should calculate total in ngOnChanges() instead
+      total += li.subtotal
+    })
+    totalControl.setValue(total)
     const customer = this.cartForm.value as Customer
     console.info(">>> CUSTOMER", customer)
   }
